84fe160264b0797dff1367c4b86bf9689e3e283d
MMXCX <mmxcx@gmail.com>
2021-11-22 07:02:29 +0300

    step by step

--- src/App.js
+++ src/App.js
@@ -9,27 +9,28 @@ import {usePosts} from "./hooks/usePosts";
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import {useFetching} from './hooks/useFetching';
+import {getPageCount, getPagesArray} from './utils/pages';
+import Pagination from './components/UI/pagination/Pagination';
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
-    const [totalCount, setTotalCount] = useState(0)
+    const [totalPages, setTotalPages] = useState(0)
     const [limit, setLimit] = useState(10)
     const [page, setPage] = useState(1)
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const response = await PostService.getAll(limit, page)
-        setPosts(response.data)
-        // console.log(response)
-        setTotalCount(parseInt(response.headers['x-total-count']))
-    })
-
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit))
+    })
 
-//1:58:36
+//2:12:39
     useEffect(() => {
-        fetchPosts().then(() => {
+        fetchPosts(limit, page).then(() => {
         })
     }, [])
 
@@ -40,6 +41,11 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id));
 
+    }
+
+    const changePage = (page) => {
+        setPage(page)
+        fetchPosts(limit, page).then(() => {})
     }
 
     return (
@@ -60,13 +66,20 @@ function App() {
             {postError &&
             <h1>Error: {postError}</h1>
             }
+            <Pagination
+                totalPages={totalPages}
+                page={page}
+                changePage={changePage}
+            />
             {isPostsLoading
-                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
-                    <Loader/>
-                </div>
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
                 : <PostList posts={sortedAndSearchedPosts} title={"Post list."} remove={removePost}/>
-
             }
+            <Pagination
+                totalPages={totalPages}
+                page={page}
+                changePage={changePage}
+            />
         </div>
     );
 }
