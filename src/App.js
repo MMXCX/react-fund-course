e9e42b6b68a68fa53a0337d3e08b9f19d89ecd51
MMXCX <mmxcx@gmail.com>
2021-11-24 22:58:50 +0300

    step by step

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React from 'react';
+import * as React from 'react';
 import './styles/App.css';
-import Posts from "./pages/Posts";
-import About from "./pages/About";
-import {BrowserRouter as Router, Route, Routes, useNavigate} from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
-//2:20:16
+import AppRouter from "./components/AppRouter";
+//2:22:22
+//TODO Learn more about "react-router-dom"
 
 function App() {
     return (
-        <Router>
-            <div className={"navbar"}>
-                <Navbar/>
-            </div>
-
-            <Routes>
-                <Route path={"/posts"} element={<Posts/>}/>
-                <Route path={"/about"} element={<About/>}/>
-                <Route path={"/*"} element={
-                    <h1>Route is Not exist!</h1>
-                }/>
-
-            </Routes>
-        </Router>
+        <>
+            <Navbar/>
+            <AppRouter/>
+        </>
     )
 }
 
--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -3,10 +3,12 @@ import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="navbar__links">
-            <Link to={"/"}>Home</Link>
-            <Link to={"/posts"}>Posts</Link>
-            <Link to={"/about"}>About</Link>
+        <div className={"navbar"}>
+            <div className="navbar__links">
+                <Link to={"/"}>Home</Link>
+                <Link to={"/posts"}>Posts</Link>
+                <Link to={"/about"}>About</Link>
+            </div>
         </div>
     );
 };
